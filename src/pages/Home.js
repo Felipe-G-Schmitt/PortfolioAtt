b311d2db37c0b53f551709e38a98eda3e8e4bfbb
Felipe-G-Schmitt <felipegabrielsc2006@gmail.com>
2025-08-24 21:44:54 -0300

    feat: add new section with tools and languages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import profile from "../assets/profile.png";
 import { IoMdMail, IoIosPin } from "react-icons/io";
-import { FaPhoneAlt, FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FaPhoneAlt, FaGithub, FaLinkedin, FaInstagram, FaHtml5, FaCss3Alt, FaJsSquare, FaPhp, FaNodeJs, FaJava, FaReact, FaFigma, } from "react-icons/fa";
+import { SiMysql, SiCplusplus, SiXampp, SiAdobephotoshop } from "react-icons/si";
+import { VscVscode } from "react-icons/vsc";
+import { LiaGit } from "react-icons/lia";
 
 function Home() {
   return (
@@ -10,7 +13,7 @@ function Home() {
         <img src={profile} alt="profile picture" className="profile"/>
         <div className="image-overlay"></div>
       </div>
-      <div className=".image-gradient" />
+      <div className="image-gradient" />
 
       <div className="infos">
         <h1>Felipe Gabriel Schmitt</h1>
@@ -50,6 +53,39 @@ function Home() {
     <section className="langTolls">
       <h1>Linguagens e ferramentas</h1>
       <div className="skills">
+        <div className="skills-track">
+          <FaHtml5 size={28}/>
+          <FaCss3Alt size={28}/>
+          <FaJsSquare size={28}/>
+          <FaPhp size={28}/>
+          <SiMysql size={28}/>
+          <FaNodeJs size={28}/>
+          <FaJava size={28}/>
+          <SiCplusplus size={28}/>
+          <FaReact size={28}/>
+          <FaReact size={28}/>
+          <VscVscode size={28}/>
+          <SiXampp size={28}/>
+          <LiaGit size={28}/>
+          <FaFigma size={28}/>
+          <SiAdobephotoshop size={28}/>
+
+          <FaHtml5 size={28}/>
+          <FaCss3Alt size={28}/>
+          <FaJsSquare size={28}/>
+          <FaPhp size={28}/>
+          <SiMysql size={28}/>
+          <FaNodeJs size={28}/>
+          <FaJava size={28}/>
+          <SiCplusplus size={28}/>
+          <FaReact size={28}/>
+          <FaReact size={28}/>
+          <VscVscode size={28}/>
+          <SiXampp size={28}/>
+          <LiaGit size={28}/>
+          <FaFigma size={28}/>
+          <SiAdobephotoshop size={28}/>
+        </div>
       </div>
     </section>
     </>
